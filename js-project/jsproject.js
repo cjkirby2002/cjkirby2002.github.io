a4789c6042e15067c6ecfa710e98ed619e589160
cjkirby2002 <clki6929@colorado.edu>
2023-12-14 12:06:31 -0700

    final

--- js-project/jsproject.js
+++ js-project/jsproject.js
@@ -1,21 +1,37 @@
-function generateRandomNumber(generatorId) {
-  // Generate a random number between 0 and 9
+function generateRandomNumber(generatorId, position) {
   var randomNumber = Math.floor(Math.random() * 10);
+  var targetId = `randomNumber${generatorId}-${position}`;
 
-  // Display the random number
-  document.getElementById(`randomNumber${generatorId}`).innerText = `Random Number: ${randomNumber}`;
+  console.log(targetId);
 
-  // Store the current random number in a variable for potential saving
-  window.currentRandomNumber = randomNumber;
+  var targetElement = document.getElementById(targetId);
+
+  if (targetElement) {
+    targetElement.textContent = `Random Number: ${randomNumber}`;
+    window.currentRandomNumber = randomNumber;
+  } else {
+    console.error(`Element with ID '${targetId}' not found.`);
+  }
 }
 
-function saveNumber(generatorId) {
+
+function saveNumber(generatorId, position) {
   if (window.currentRandomNumber !== undefined) {
     // Store the current random number in the saved numbers list
     var savedNumberList = document.getElementById('savedNumberList');
-    var listItem = document.createElement("li");
-    listItem.textContent = `${window.currentRandomNumber}`;
-    savedNumberList.appendChild(listItem);
+
+    // Get the list item at the specified position
+    var listItem = document.getElementById(`savedNumber${generatorId}-${position}`);
+
+    // If the list item exists, update its content; otherwise, create a new list item
+    if (listItem) {
+      listItem.textContent = `${window.currentRandomNumber}`;
+    } else {
+      listItem = document.createElement("li");
+      listItem.id = `savedNumber${generatorId}-${position}`;
+      listItem.textContent = `${window.currentRandomNumber}`;
+      savedNumberList.appendChild(listItem);
+    }
 
     // Clear the current random number after saving
     window.currentRandomNumber = undefined;
